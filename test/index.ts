8a1db91578df2a359adff6da36c1a0c582b52df5
Michael Danilov <michael@danilov.me>
2020-06-06 10:46:22 +0300

    Fix vulnerabilities (#5)
    * update deps
    
    * update tests
    
    * bump version

--- test/index.ts
+++ test/index.ts
@@ -4,40 +4,40 @@ import test from 'ava';
 import michaeldanilov from '../index';
 
 // English
-test('en name', t => {
+test('en name', (t) => {
   t.is(michaeldanilov.name, 'Michael Danilov');
 });
 
-test('en firstName', t => {
+test('en firstName', (t) => {
   t.is(michaeldanilov.names.firstName, 'Michael');
 });
 
-test('en lastName', t => {
+test('en lastName', (t) => {
   t.is(michaeldanilov.names.lastName, 'Danilov');
 });
 
-test('en location', t => {
+test('en location', (t) => {
   t.is(michaeldanilov.location, 'Moscow, Russia');
 });
 
 // Russian
-test('ru name', t => {
+test('ru name', (t) => {
   t.is(michaeldanilov.ru.name, 'Михаил Данилов');
 });
 
-test('ru firstName', t => {
+test('ru firstName', (t) => {
   t.is(michaeldanilov.ru.names.firstName, 'Михаил');
 });
 
-test('ru lastName', t => {
+test('ru lastName', (t) => {
   t.is(michaeldanilov.ru.names.lastName, 'Данилов');
 });
 
-test('ru location', t => {
+test('ru location', (t) => {
   t.is(michaeldanilov.ru.location, 'Москва, Россия');
 });
 
 // Common
-test('common homepage', t => {
+test('common homepage', (t) => {
   t.is(michaeldanilov.homepage, 'https://danilov.me');
 });
